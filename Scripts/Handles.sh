3694730b5d80038438c13c1d4c846d75649b9e85
OlaveV <heifu303@gmail.com>
2026-01-05 22:08:13 +0800

    Add files via upload

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -2,43 +2,24 @@
 
 PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
-#修改qca-nss-drv启动顺序
+# 修改 NSS 相关组件启动顺序以优化性能
 NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
-if [ -f "$NSS_DRV" ]; then
-	echo " "
+[ -f "$NSS_DRV" ] && sed -i 's/START=.*/START=85/g' "$NSS_DRV" && echo "qca-nss-drv fixed"
 
-	sed -i 's/START=.*/START=85/g' $NSS_DRV
-
-	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
-fi
-
-#修改qca-nss-pbuf启动顺序
 NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
-if [ -f "$NSS_PBUF" ]; then
-	echo " "
+[ -f "$NSS_PBUF" ] && sed -i 's/START=.*/START=86/g' "$NSS_PBUF" && echo "qca-nss-pbuf fixed"
 
-	sed -i 's/START=.*/START=86/g' $NSS_PBUF
-
-	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
-fi
-
-#修复Rust编译失败
+# 修复 Rust 编译环境
 RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
 if [ -f "$RUST_FILE" ]; then
-	echo " "
-
-	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
-
-	cd $PKG_PATH && echo "rust has been fixed!"
+	sed -i 's/ci-llvm=true/ci-llvm=false/g' "$RUST_FILE"
 fi
 
-#修复DiskMan编译失败
+# 修复 DiskMan 及其文件系统依赖
 DM_FILE="./luci-app-diskman/applications/luci-app-diskman/Makefile"
 if [ -f "$DM_FILE" ]; then
-	echo " "
-
-	sed -i 's/fs-ntfs/fs-ntfs3/g' $DM_FILE
-	sed -i '/ntfs-3g-utils /d' $DM_FILE
-
-	cd $PKG_PATH && echo "diskman has been fixed!"
+	sed -i 's/fs-ntfs/fs-ntfs3/g' "$DM_FILE"
+	sed -i '/ntfs-3g-utils /d' "$DM_FILE"
+	# 额外修复：确保 automount 使用 ntfs3
+	[ -f "./automount/files/15-automount" ] && sed -i 's/ntfs/ntfs3/g' "./automount/files/15-automount"
 fi
--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,66 +1,50 @@
 #!/bin/bash
 
-#修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-#修改immortalwrt.lan关联IP
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-#添加编译日期标识
-#sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
+# [修改点] 注释掉下方行，以恢复 Argon 原生登录界面，不再强制替换默认主题指向
+# sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
+# 优化无线配置
 WIFI_SH=$(find ./target/linux/{mediatek/filogic,qualcommax}/base-files/etc/uci-defaults/ -type f -name "*set-wireless.sh" 2>/dev/null)
 WIFI_UC="./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
+
 if [ -f "$WIFI_SH" ]; then
-	#修改WIFI名称
-	sed -i "s/BASE_SSID='.*'/BASE_SSID='$WRT_SSID'/g" $WIFI_SH
-	#修改WIFI密码
-	sed -i "s/BASE_WORD='.*'/BASE_WORD='$WRT_WORD'/g" $WIFI_SH
+	sed -i "s/BASE_SSID='.*'/BASE_SSID='$WRT_SSID'/g" "$WIFI_SH"
+	sed -i "s/BASE_WORD='.*'/BASE_WORD='$WRT_WORD'/g" "$WIFI_SH"
 elif [ -f "$WIFI_UC" ]; then
-	#修改WIFI名称
-	sed -i "s/ssid='.*'/ssid='$WRT_SSID'/g" $WIFI_UC
-	#修改WIFI密码
-	sed -i "s/key='.*'/key='$WRT_WORD'/g" $WIFI_UC
-	#修改WIFI地区
-	sed -i "s/country='.*'/country='CN'/g" $WIFI_UC
-	#修改WIFI加密
-	sed -i "s/encryption='.*'/encryption='psk2+ccmp'/g" $WIFI_UC
+	sed -i "s/ssid='.*'/ssid='$WRT_SSID'/g" "$WIFI_UC"
+	sed -i "s/key='.*'/key='$WRT_WORD'/g" "$WIFI_UC"
+	sed -i "s/country='.*'/country='CN'/g" "$WIFI_UC"
+	sed -i "s/htmode='.*'/htmode='HE160'/g" "$WIFI_UC"
+	sed -i "s/mu_beamformer='0'/mu_beamformer='1'/g" "$WIFI_UC"
+	sed -i "s/encryption='.*'/encryption='psk2+ccmp'/g" "$WIFI_UC"
 fi
 
+# 系统配置修改
 CFG_FILE="./package/base-files/files/bin/config_generate"
-#修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
-#修改默认主机名
-sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" "$CFG_FILE"
+sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" "$CFG_FILE"
 
-#配置文件修改
+# 增加内核优化参数
+echo "net.netfilter.nf_conntrack_max=65535" >> ./package/base-files/files/etc/sysctl.conf
+echo "net.core.default_qdisc=fq_codel" >> ./package/base-files/files/etc/sysctl.conf
+
+# 强制开启核心插件
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
-#echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-#echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
-#手动调整的插件
-if [ -n "$WRT_PACKAGE" ]; then
-	echo -e "$WRT_PACKAGE" >> ./.config
-fi
-
-#高通平台调整
-DTS_PATH="./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/"
+# 高通平台特定优化
 if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
-	#取消nss相关feed
 	echo "CONFIG_FEED_nss_packages=n" >> ./.config
 	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
-	#开启sqm-nss插件
-	#echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
-	#echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
-	#设置NSS版本
-	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
+	
 	if [[ "${WRT_CONFIG,,}" == *"ipq50"* ]]; then
 		echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
 	else
 		echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
 	fi
-	#无WIFI配置调整Q6大小
+	
 	if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
-		find $DTS_PATH -type f ! -iname '*nowifi*' -exec sed -i 's/ipq\(6018\|8074\).dtsi/ipq\1-nowifi.dtsi/g' {} +
-		echo "qualcommax set up nowifi successfully!"
+		DTS_PATH="./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/"
+		find "$DTS_PATH" -type f ! -iname '*nowifi*' -exec sed -i 's/ipq\(6018\|8074\).dtsi/ipq\1-nowifi.dtsi/g' {} +
 	fi
 fi
