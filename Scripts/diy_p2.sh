9ba9015230543f7ef94597adec541151a7686779
legna05 <1062723226@qq.com>
2026-02-16 17:28:54 +0800

    modify

---
+++ Scripts/diy_p2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+feeds_path="/home/runner/work/ZN-M2-VIKINGYFY/ZN-M2-VIKINGYFY/wrt/scripts/feeds"
+
+#优先安装 passwall 源
+$feeds_path install -a -f -p passwall_packages
+$feeds_path install -a -f -p passwall_luci
+#优先安装 ddns-go 源
+#./scripts/feeds update ddns-go
+$feeds_path install -a -f -p ddns_go
